b2dacd4f0fdb4937c192be5cd89db722ba89ebf4
Crafter2012 <Crafter2012@gmail.com>
2013-05-29 03:50:36 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RSS::Application.config.secret_token = '43124b4c852ed4d1c98006c5e9a9d7de48607925572e1c7d6316e3e4ca47c6f2224f560f655d1cf532d53c59bc5c3d60ee3e0713bd6bc65ca7196965dae1a88b'
